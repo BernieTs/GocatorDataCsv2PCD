2399b283f2cb9a634b501bbd1a3b3ca75801c1be
BernieTsou <tsoubernie@gmail.com>
2020-06-29 16:10:21 +0800

    實際讀取Gocator儲存下來的.csv檔

--- main.cpp
+++ main.cpp
@@ -8,31 +8,67 @@
 #include <QStringList>
 #include <QDir>
 
+#define RowCountNameLocation 25
+#define RowCountValueLocation 26
+#define XListLocation 27
+#define DataRowBegin 28
+
+
+class Point
+{
+public:
+    Point(){}
+    Point(double x, double y, double z)
+    {xx = x; yy = y; zz = z;}
+
+    void Print()
+    {qDebug()<<"x:"<<xx<<" y:"<<yy<<" z:"<<zz;}
+
+public:
+    double xx;
+    double yy;
+    double zz;
+};
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-
-    // prepare data that you want to save to csv-file
-    QStringList strList;
-    strList << "one" << "two" << "three";
-
-    QtCSV::StringData strData;
-    strData.addRow(strList);
-    strData.addEmptyRow();
-    strData << strList << "this is the last row";
-
-    // write to file
-    QString filePath = QDir::currentPath() + "/test.csv";
-    QtCSV::Writer::write(filePath, strData);
+    QString filePath = QDir::currentPath() + "/replay_40959_2020-6-24.csv";
 
     // read data from file
     QList<QStringList> readData = QtCSV::Reader::readToList(filePath);
-    for ( int i = 0; i < readData.size(); ++i )
+
+    //data row count name和value位置
+    QString rowCntName = readData.at(RowCountNameLocation).at(6);
+    int rowCnt = readData.at(RowCountValueLocation).at(6).toInt();
+
+    qDebug()<<rowCntName<<":"<<rowCnt;
+
+    QStringList listX = readData.at(XListLocation);
+    std::vector<Point> vPoints;
+
+    for ( int i = 0; i <rowCnt; ++i )
     {
-        qDebug() << readData.at(i).join(",");
+        QStringList listY = readData.at(DataRowBegin + i);
+
+        for(int j=1; j<listY.size(); j++)
+        {
+            if(!listY.at(j).isEmpty())
+            {
+                Point p;
+                p.xx = listX.at(j).toDouble();
+                p.yy = listY.at(0).toDouble();
+                p.zz = listY.at(j).toDouble();
+
+                vPoints.push_back(p);
+            }
+        }
     }
 
+    qDebug()<<"vPoints.size():"<<vPoints.size();
 
-    return a.exec();
+    for (int var = 0; var < 10; ++var) {
+        vPoints.at(var).Print();
+    }
+
+    return 0;
 }
